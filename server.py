84505545656270064d702bedc61b1f3485bc38b9
BihackerViktor <bihacker.viktor@gmail.com>
2019-04-29 16:21:09 +0200

    images

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -20,7 +20,7 @@ def widow():
 
 if __name__ == '__main__':
     app.run(
-     host="0.0.0.0",
-        port="5000",
+        host="0.0.0.0",
+        port=5000,
         debug=True,
     )
