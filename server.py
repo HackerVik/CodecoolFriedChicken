45f9ad178e7675d0e8784343fc18b78a7a112409
GaborPap <ppgabor@gmail.com>
2019-04-29 13:54:18 +0200

    Added python server file and index html.

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/memory')
+def memory():
+    pass
+
+
+@app.route('/widow')
+def widow():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+    )
