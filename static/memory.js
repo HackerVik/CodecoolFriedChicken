a48a1804c275dbf5991bbee4aa467d4e3519032b
GaborPap <ppgabor@gmail.com>
2019-04-30 11:05:00 +0200

    refactor

--- static/memory.js
+++ static/memory.js
@@ -71,24 +71,17 @@ function getActualArray() {
 
 
 function shuffle() {
-    array = JSON.parse(sessionStorage.getItem("originalArray"));
+    let array = JSON.parse(sessionStorage.getItem("originalArray"));
     let currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
-        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
 
-
         changeImageSource(array);
-
 
         return array;
     }
