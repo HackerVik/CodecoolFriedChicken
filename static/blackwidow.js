952e3d116f1468d24ac395db5124d4dc80301d1b
Márton Tévald <marton.tevald@gmail.com>
2019-04-30 10:37:52 +0200

    shuffle deck

---
+++ static/blackwidow.js
@@ -0,0 +1,34 @@
+
+
+//for difficulty setting later
+function getDeck(num) {
+    let deck = [];
+    for (let j = 0; j < num; j++) {
+        for (let i = 1; i < 14; i++) {
+            deck.push(i.toString());
+        }
+    }
+    return deck;
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+shuffle(getDeck(8));
